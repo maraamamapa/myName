2e0ebef33c2ac11a73235de50d57612e21b48b8f
Ampa <ampa_margalef@hotmail.com>
2016-10-05 13:01:17 +0200

    miNombre

---
+++ ampa.php
@@ -0,0 +1,13 @@
+//fichero index.php
+//creado por Paco Gomez
+<html>
+  <head>
+      <title>ACTIVIDAD 03: Git y PHP</title>
+      <?php
+        echo "Ampa"."<br>";
+        echo "Margalef"."<br>";
+        print "Cervero";
+      ?>
+  </head>
+  <body>
+</html>
